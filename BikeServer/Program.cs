d3d46003fc9b82dd1c6876b69213cab500633a69
Philip Lind Jensen <jogjensen@hotmail.com>
2020-09-15 10:31:33 +0200

    Add project files.

---
+++ BikeServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BikeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
